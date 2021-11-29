b93f337b1192f5dbc18f702a91f3a891becdffc0
RodrigoJesusSantistebanPachari <58257135+RodrigoJesusSantistebanPachari@users.noreply.github.com>
2021-11-29 01:41:59 -0500

    Update Unique Paths II.cpp

--- Práctica 17-11-2021/Unique Paths II.cpp
+++ Práctica 17-11-2021/Unique Paths II.cpp
@@ -3,11 +3,39 @@
 
 using namespace std;
 
-int minCostClimbingStairs(vector<int>& cost) {
-    for (int i = cost.size()-3; i>=0; i--)
-        cost[i] += min(cost[i+1], cost[i+2]);
-    return min(cost[0], cost[1]);
+int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        if (m == 0) {
+            return 0;
+        }
+        int n = obstacleGrid[0].size();
+        if (n == 0) {
+            return 0;
+        }
+        vector<vector<int>> cache(m, vector<int>(n, -1));
+        return dfs(obstacleGrid, cache, 0, 0, m, n);
 }
+
+int dfs(vector<vector<int>>& obstacleGrid, vector<vector<int>>& cache, int i, int j, int m, int n){
+        if (obstacleGrid[i][j] == 1) {
+            return 0;
+        }
+        if (i + 1 == m && j + 1 == n) {
+            return 1;
+        }
+        if (cache[i][j] != -1) {
+            return cache[i][j];
+        }
+        int paths = 0;
+        if (i + 1 < m) {
+            paths += dfs(obstacleGrid, cache, i + 1, j, m, n);
+        }
+        if (j + 1 < n) {
+            paths += dfs(obstacleGrid, cache, i, j + 1, m, n);
+        }
+        cache[i][j] = paths;
+        return paths;
+    }
 
 
 int main(){
@@ -19,6 +47,6 @@ int main(){
 	for(int i=0; i<n; i++)
 		cin>>v[i];
 	
-	cout<<"Output: "<<minCostClimbingStairs(v);
+	cout<<"Output: "<<uniquePathsWithObstacles(v);
 	return 0;
 }
